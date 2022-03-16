cb50fcbbf31752e7b992760b59855bae82e0dbca
Aiden125 <moan125@naver.com>
2022-03-17 01:14:34 +0900

    22.03.16new

---
+++ Source/01_java/st02_var/src/com/study/Var/Ex01.java
@@ -0,0 +1,14 @@
+package com.study.Var;
+
+public class Ex01 {
+	public static void main(String[] args) {
+		double Korean = 80.3;
+		double English = 90.6;
+		double Math = 99.9;
+		System.out.println("Korean="+Korean);
+		System.out.println("English="+English);
+		System.out.println("Math="+Math);
+		System.out.println("ÃÑÁ¡="+(Korean+English+Math));
+		System.out.println("Æò±Õ="+(Korean+English+Math)/3);
+	}
+}
---
+++ Source/01_java/st04_control/src/com/st/control/Ex01.java
@@ -0,0 +1,20 @@
+package com.st.control;
+
+import java.util.Scanner;
+//수를 입력받아 절대값으로 변환하시오
+public class Ex01 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num1;
+		System.out.println("수를 입력하시오");
+		num1 = sc.nextInt();
+		if (num1 >= 0) {
+			System.out.println(num1);
+		} else {
+			System.out.println(num1*-1);
+		}
+		sc.close();
+	}
+
+}

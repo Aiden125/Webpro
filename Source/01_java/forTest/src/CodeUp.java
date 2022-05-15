74e838fa11a141fc78b4722e0c430b477caeadec
Aiden125 <moan125@naver.com>
2022-05-15 19:02:42 +0900

    22.05.15(2)

---
+++ Source/01_java/forTest/src/CodeUp.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class CodeUp {
+	public static void main(String[] args) {
+		double x;
+		Scanner sc = new Scanner(System.in);
+		x = sc.nextDouble();
+		System.out.printf("%.11f",x);
+	}
+}

6f57d65a361b1b1b6001d0115a4d111a61bee486
Aiden125 <moan125@naver.com>
2022-05-18 09:09:34 +0900

    22.05.18

---
+++ Source/01_java/z_baekjoon/src/test_step1_2588.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class test_step1_2588 {
+	public static void main(String[] args) {
+		int x,y;
+		String yStr;
+		Scanner sc = new Scanner(System.in);
+		x = sc.nextInt();
+		yStr = sc.next();
+		int a1 = x * Integer.parseInt((yStr.indexOf(2)));
+	}
+}
+

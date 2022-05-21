9a1c5ed9b0ba03ca88680059705ec302bea6fe45
Aiden125 <moan125@naver.com>
2022-05-21 23:11:52 +0900

    22.05.21

---
+++ Source/01_java/z_baekjoon/src/test_step2_9498.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class test_step2_9498 {
+	public static void main(String[] args) {
+		int x;
+		Scanner sc = new Scanner(System.in);
+		x = sc.nextInt();
+		if(90<x && ) {
+			System.out.println(">");
+		}else if(x<y) {
+			System.out.println("<");
+		}else {
+			System.out.println("==");
+		}
+	}
+}

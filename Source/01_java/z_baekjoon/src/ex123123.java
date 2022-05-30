61fb09e6b4dcf69069bc8dbe21dc62e69530fa18
Aiden125 <moan125@naver.com>
2022-05-30 20:20:47 +0900

    22.05.30(3)

--- Source/01_java/z_baekjoon/src/ex123123.java
+++ Source/01_java/z_baekjoon/src/ex123123.java
@@ -1,6 +1,12 @@
+import java.util.Arrays;
 
 public class ex123123 {
 	public static void main(String[] args) {
-		System.out.println((50+20)/60);
+		int[] arr = {15,2,4};
+		Arrays.sort(arr);
+		int a = getMedian(arr);
+		for(int i=0; i<arr.length; i++) {
+			System.out.println(arr[i]);
+		}
 	}
 }

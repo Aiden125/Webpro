9eb70b4980b8a47397cb86711169e510c3d13b5d
Aiden125 <moan125@naver.com>
2022-06-11 19:14:11 +0900

    22.06.11

---
+++ Source/01_java/z_baekjoon/src/test_step3_1110.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class test_step3_1110 {
+	public static void main(String[] args) {
+		int t = 0;
+		Scanner sc = new Scanner(System.in);
+		int startx = sc.nextInt();
+		int newx = startx;
+		
+		while(true) {
+			int left = newx / 10;
+			int right = newx%10;
+			newx = (right*10)+(left+right)%10;
+			t++;
+			if(newx == startx) {
+				break;
+			}
+		}
+		System.out.println(t);
+	}
+}
---
+++ Source/01_java/z_baekjoon/src/test_step4_10818.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class test_step4_10818 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+		int a, count = 0, min = 1000001, max= -1000001;
+		
+		count = Integer.parseInt(br.readLine());
+		st = new StringTokenizer(br.readLine());
+		
+		for(int i=0; i<count; i++) {
+			a = Integer.parseInt(st.nextToken());
+			if(a<min) {
+				min = a;
+			}
+			if(a>max) {
+				max = a;
+			}
+		}
+		bw.write(min+" "+max);
+		bw.close();
+	}
+}

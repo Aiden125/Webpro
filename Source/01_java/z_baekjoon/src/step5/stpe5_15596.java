89839ffc8acded5250f304acd9e223f0808e7a7a
Aiden125 <moan125@naver.com>
2022-06-29 09:25:25 +0900

    22.06.29(2)

---
+++ Source/01_java/z_baekjoon/src/step5/stpe5_15596.java
@@ -0,0 +1,14 @@
+/*
+package step5;
+Class Test{
+	long sum(int[] a) {
+		long sum = 0;
+		
+		for(i=0; i<a.length; i++) {
+			sum += a[i];
+		}
+		
+		return sum;
+	}
+}
+*/
---
+++ Source/01_java/z_baekjoon/src/step6_11654.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class step6_11654 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		char c = sc.next().charAt(0);
+		int a = (int) c;
+		System.out.println(a);
+		sc.close();
+	}
+}

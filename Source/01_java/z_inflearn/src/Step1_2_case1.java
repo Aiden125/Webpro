74f8349089c310d85c6dcf4142fa4ebf3ac1c89d
Aiden125 <moan125@naver.com>
2022-07-29 21:30:02 +0900

    22.07.29

---
+++ Source/01_java/z_inflearn/src/Step1_2_case1.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+// 대문자와 소문자가 같이 존재하는 문자열을 입력받아
+// 대문자는 소문자로 소문자는 대문자로 변환하여 출력하는 프로그램을 작성하세요.
+public class Step1_2_case1 {
+	public String solution(String str) {
+		String answer = "";
+		for(char x : str.toCharArray()) {
+			if(x>=97 && x<=122) {
+				answer += (char)(x-32);
+			}else {
+				answer += (char)(x+32);
+			}
+		}
+		
+		return answer;
+	}
+	
+	public static void main(String[] args) {
+		Step1_2_case1 T = new Step1_2_case1();
+		Scanner sc = new Scanner(System.in);
+		String str = sc.next();
+		System.out.println(T.solution(str));
+	}
+}
---
+++ Source/01_java/z_inflearn/src/Step5_1_case1.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Step5_1_case1 {
+	public static void main(String[] args) {
+		Step5_1_case1 T = new Step5_1_case1();
+		Scanner sc = new Scanner(System.in);
+		String str = sc.next();
+		System.out.println(T.solution(str));
+	}
+	
+	
+	public String solution(String str) {
+		String answer = "YES";
+		Stack<Character> st = new Stack<>();
+		for(char x : str.toCharArray()) {
+			if(x == '(') {
+				st.push(x);
+			}else {
+				if(st.isEmpty()) { // 닫는 괄호가 많은 경우
+					return "NO";
+				}else {
+					st.pop();
+				}
+			}
+		}
+		
+		if(!st.isEmpty()) { // 다 돌고도 여는 괄호가 남아있는 경우
+			return "NO";
+		}
+		return answer;
+	}
+}

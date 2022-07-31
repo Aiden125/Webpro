fb02c8a9cbc3eafb9f987e7f08bc9e569a41f5ac
Aiden125 <moan125@naver.com>
2022-07-31 14:51:09 +0900

    22.07.31

---
+++ Source/01_java/z_inflearn/src/Step5_5_case2.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+import java.util.Stack;
+// 1. 스택을 만든다
+// 2. 여는 괄호 푸시
+// 3. 닫는 괄호는 레이저의 닫는것인지, 쇠막대기의 끝인지(스택의 상단이 '('이면 레이저)
+
+public class Step5_5_case2 {
+	public static void main(String[] args) {
+		Step5_5_case2 T = new Step5_5_case2();
+		Scanner sc = new Scanner(System.in);
+		String str = sc.next();
+		System.out.println(T.solution(str));
+	}
+
+	public int solution(String str) {
+		int answer = 0;
+		Stack<Character> stack = new Stack<>();
+		for (int i=0; i<str.length(); i++) {
+			if (str.charAt(i) == '(') { // 여는 괄호면 일단 넣기(레이저 인지 쇠막대기인지 모름)
+				stack.push('(');
+			} else {
+				if (str.charAt(i-1) == '(') {// case1. 레이저인 경우
+					stack.pop();
+					answer += stack.size();
+				} else {// case2. 쇠막대기 끝인 경우
+					stack.pop();
+					answer++;
+				}
+			}
+		}
+
+		return answer;
+	}
+}
--- Source/01_java/z_inflearn/src/step5/Step5_4_case2.java
+++ Source/01_java/z_inflearn/src/step5/Step5_4_case2.java
@@ -1,23 +1,21 @@
-package review_yet;
+package step5;
 import java.util.Scanner;
 import java.util.Stack;
 
-public class Step5_4_case1 {
+public class Step5_4_case2 {
 	public static void main(String[] args) {
-		Step5_4_case1 T = new Step5_4_case1();
+		Step5_4_case2 T = new Step5_4_case2();
 		Scanner sc = new Scanner(System.in);
 		String str = sc.next();
 		System.out.println(T.solution(str));
 	}
-	
-	
 	
 	public int solution(String str) {
 		int answer = 0;
 		Stack<Integer> stack = new Stack<>();
 		int lt, rt;
 		for(Character x : str.toCharArray()) {
-			if((int) x < 58 && (int)x >=48) {
+			if(Character.isDigit(x)) { // x가 숫자냐?
 				stack.push(x - 48);
 			}else {
 				rt = stack.pop();

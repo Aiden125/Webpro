ecfe353f9c44d7d5f296ce511e2fce668e53a908
Aiden125 <moan125@naver.com>
2022-08-04 18:26:38 +0900

    22.08.04

---
+++ Source/01_java/z_inflearn/src/review_yet/Step3_2_case1.java
@@ -0,0 +1,26 @@
+package review_yet;
+import java.util.Scanner;
+
+public class Step3_2_case1 {
+	public int solution(int n, int[] arr) {
+		int answer=0;
+		int max = -1;
+		for(int i=0; i<n; i++) {
+			if(arr[i]>max) {
+				answer++;
+				max = arr[i];
+			}
+		}
+		return answer;
+	}
+	public static void main(String[] args) {
+		Step3_2_case1 T = new Step3_2_case1();
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] arr = new int[n];
+		for(int i=0; i<n; i++) {
+			arr[i] = sc.nextInt();
+		}
+		System.out.println(T.solution(n, arr));
+	}
+}

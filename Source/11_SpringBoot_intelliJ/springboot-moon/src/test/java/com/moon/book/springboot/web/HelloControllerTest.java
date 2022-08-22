3e4a7020b2ce33854c91db3540c06989f258e024
Aiden125 <moan125@naver.com>
2022-08-22 20:29:39 +0900

    22.08.22

--- Source/11_SpringBoot_intelliJ/springboot-moon/src/test/java/com/moon/book/springboot/web/HelloControllerTest.java
+++ Source/11_SpringBoot_intelliJ/springboot-moon/src/test/java/com/moon/book/springboot/web/HelloControllerTest.java
@@ -6,9 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class) // 테스트 진행 시 Junit에 내장된 실행자 외에 다른 실행자를 실행
 @WebMvcTest(controllers = HelloController.class) // Web(Spring MVC)에 집중할 수 있도록 돕는 어노테이션
@@ -26,4 +27,16 @@ public class HelloControllerTest {
                 .andExpect(content().string(hello)); // 응답 본문의 내용을 검토(여기선 hello 리턴이 맞는지)
     }
 
+    @Test
+    public void helloDto가_리턴된다() throws Exception{
+        String name = "hello";
+        int amount = 1000;
+        mvc.perform(
+                get("/hello/dto")
+                        .param("name", name)
+                        .param("amount", String.valueOf(amount)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is(name))) // $를 기준으로 필드명 명시
+                .andExpect(jsonPath("$.amount", is(amount)));
+    }
 }
